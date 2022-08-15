6e96c31d3b88b7a8ad05948eba49b5463a31895d
Vitor-Salgado <110491805+Vitor-Salgado@users.noreply.github.com>
2022-08-15 16:02:09 -0300

    attt

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+
+console.log(`Sua velocidade e de ${vel}km/h`)
+if(vel > 60){
+    console.log('Voce foi multado por excesso de velocidade')
+}
+console.log(`Use sempre o cinto de segurança`)
---
+++ aula011/ex009.js
@@ -0,0 +1,4 @@
+var pais = 'eul'
+if(pais == 'brasil'){console.log('Você é Brasileiro')
+}
+else(console.log('Você é Estrangeiro'))
